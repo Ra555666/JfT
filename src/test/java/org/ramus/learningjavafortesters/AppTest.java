c56cff38102937358cc699c18e74cfccb3171250
Kola Ra <santaramus@gmail.com>
2019-09-19 15:31:47 +0300

    first commit

---
+++ src/test/java/org/ramus/learningjavafortesters/AppTest.java
@@ -0,0 +1,20 @@
+package org.ramus.learningjavafortesters;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
