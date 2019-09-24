b1b11ae9dd411a9b3eb9b878769cb41921620a18
Kola Ra <santaramus@gmail.com>
2019-09-24 22:50:48 +0300

    add optional task2

--- src/main/java/org/ramus/javafundamentals/OptionalTask2.java
+++ src/main/java/org/ramus/javafundamentals/OptionalTask2.java
@@ -1,4 +1,32 @@
 package org.ramus.javafundamentals;
 
+import java.util.Scanner;
+
 public class OptionalTask2 {
+    public static void main(String[] args) {
+        int width = new Scanner(System.in).nextInt();
+        int hight = new Scanner(System.in).nextInt();
+        mean(matrixFilling(width, hight));
+    }
+
+    public static int[][] matrixFilling(int width, int hight) {
+        int[][] array = new int[hight][width];
+        for (int i = 0; i < hight; i++) {
+            for (int j = 0; j < width; j++) {
+                array[i][j] = 0 + (int) (Math.random() * 99);
+            }
+        }
+        return array;
+    }
+
+    public static void mean(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+            System.out.println((double) sum / matrix[i].length);
+            sum = 0;
+        }
+    }
 }
