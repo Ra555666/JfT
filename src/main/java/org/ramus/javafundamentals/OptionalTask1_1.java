b8b6f803f19ca2417adc9f3f9c4fbdf044a35c72
Kola Ra <santaramus@gmail.com>
2019-09-24 17:53:26 +0300

    add optional task 1-2

--- src/main/java/org/ramus/javafundamentals/OptionalTask1_1.java
+++ src/main/java/org/ramus/javafundamentals/OptionalTask1_1.java
@@ -8,6 +8,7 @@ public class OptionalTask1_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int temp;
+        int average;
 
         System.out.println("How much number do you want input?");
 
@@ -17,12 +18,13 @@ public class OptionalTask1_1 {
 
         System.out.println("Input your numbers");
 
+        //filling array by numbers
         for (int i = 0; i < numbers; i++){
             userNumbers[i] = input.nextInt();
         }
 
-//        System.out.println(Arrays.toString(userNumbers));
 
+        //sort numbers in array
         for (int i = 1; i < userNumbers.length; i++){
             for (int j = i; j > 0; j--){
                 if (userNumbers[j] < userNumbers[j - 1]){
@@ -32,17 +34,30 @@ public class OptionalTask1_1 {
                 }
             }
         }
+
+        //output numbers by length from min to max
         for (int i = 0; i < userNumbers.length; i++){
             System.out.println(userNumbers[i]);
         }
+
+        //output numbers by length from max to min
+        for(int i = userNumbers.length - 1; i>=0; i--){
+            System.out.println(userNumbers[i]);
+        }
+
+
+        //output shortest number from array
         System.out.println("short number is");
         System.out.println(userNumbers[0]);
         System.out.println("it length is");
         System.out.println(Integer.toString(userNumbers[0]).length());
 
+        //output longest number from array
         System.out.println("largest number is");
         System.out.println(userNumbers[userNumbers.length-1]);
         System.out.println("it length is");
         System.out.println(Integer.toString(userNumbers[userNumbers.length-1]).length());
+
+
     }
 }
