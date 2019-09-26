3e95af32c7b4ac82ceeacea973dfe5237b40d0a8
Kola Ra <santaramus@gmail.com>
2019-09-26 18:59:08 +0300

    add Main Task almost done

--- src/main/java/org/ramus/javafundamentals/mainTask/MTaskFundamentals.java
+++ src/main/java/org/ramus/javafundamentals/mainTask/MTaskFundamentals.java
@@ -1,21 +1,73 @@
 package org.ramus.javafundamentals.mainTask;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MTaskFundamentals {
-//    String name;
-    Scanner read;
 
-    public void greeting(String name){
-
+    public static void greeting(String name){
         System.out.println("Greeting " + name);
     }
 
-
-    public void revert(String name) {
+    public static void revert(String name) {
         StringBuilder sb = new StringBuilder(name);
+        System.out.print("Now your name is ");
+        name = sb.reverse().toString().toLowerCase();
+        System.out.println(name.substring(0,1).toUpperCase() + name.substring(1,name.length()));
+    }
 
-        System.out.print("Reversed name is: ");
-        System.out.println(sb.reverse().toString());
+    public static void workingWithNumbers(int quality){
+        int[] arr = new int[quality];
+        System.out.println("With perenos");
+        for (int i = 0; i < quality; i++){
+            arr[i] = (int)(Math.random()*11);
+            System.out.println(arr[i]);
+        }
+        System.out.println("Without perenos");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void summingNumbers(int quality){
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[quality];
+        int sum = 0;
+        for (int i = 0; i < quality; i++){
+            arr[i] = in.nextInt();
+            sum +=arr[i];
+        }
+        System.out.println("Sum of your number is " + sum);
+    }
+
+    public static String whatIsMonthIs(int monthNumber){
+        String month;
+        switch (monthNumber){
+            case 1: month = "January";
+                break;
+            case 2: month = "February";
+                break;
+            case 3: month = "March";
+                break;
+            case 4: month = "April";
+                break;
+            case 5: month = "May";
+                break;
+            case 6: month = "June";
+                break;
+            case 7: month = "July";
+                break;
+            case 8: month = "August";
+                break;
+            case 9: month = "September";
+                break;
+            case 10: month = "October";
+                break;
+            case 11: month = "November";
+                break;
+            case 12: month = "December";
+                break;
+            default: month = "Try again";
+        }
+        return month;
     }
 }
--- src/main/java/org/ramus/javafundamentals/mainTask/TestMTaskFundamentals.java
+++ src/main/java/org/ramus/javafundamentals/mainTask/TestMTaskFundamentals.java
@@ -6,11 +6,26 @@ public class TestMTaskFundamentals {
     public static void main(String[] args) {
         MTaskFundamentals mainTask = new MTaskFundamentals();
         Scanner input = new Scanner(System.in);
+        String month;
 
-        System.out.print("Enter your name: ");
-        String userName = input.nextLine();
-        mainTask.greeting(userName);
 
-        mainTask.revert(userName);
+//        System.out.println("What is your name?");
+//        String userName = input.nextLine();
+//        mainTask.greeting(userName);
+//        mainTask.revert(userName);
+//
+//        System.out.println("Input required quantity of numbers: ");
+//        int quality = input.nextInt();
+//        mainTask.workingWithNumbers(quality);
+//
+//        System.out.println(" Sum of which numbers you want to know ");
+//        int numForSum = input.nextInt();
+//        mainTask.summingNumbers(numForSum);
+
+        System.out.println("What month number is ? ");
+        int monthNumber = input.nextInt();
+        month = mainTask.whatIsMonthIs(monthNumber);
+        System.out.println("Your month is "+ month);
+
     }
 }
