1afa5cb66a5425c39678384006aeafa575880877
Kola Ra <santaramus@gmail.com>
2019-10-01 16:47:27 +0300

    fix main task

--- src/main/java/org/ramus/javafundamentals/mainTask/TestMTaskFundamentals.java
+++ src/main/java/org/ramus/javafundamentals/mainTask/TestMTaskFundamentals.java
@@ -7,33 +7,45 @@ public class TestMTaskFundamentals {
         MTaskFundamentals mainTask = new MTaskFundamentals();
         Scanner input = new Scanner(System.in);
         String month;
-        boolean monthActive = true;
+        int monthNum = 0;
+        boolean monthActive;
 
 
-//        System.out.println("What is your name?");
-//        String userName = input.nextLine();
-//        mainTask.greeting(userName);
-//        mainTask.revert(userName);
-//
-//        System.out.println("Input required quantity of numbers: ");
-//        int quality = input.nextInt();
-//        mainTask.workingWithNumbers(quality);
-//
-//        System.out.println(" Sum of which numbers you want to know ");
-//        int numForSum = input.nextInt();
-//        mainTask.summingNumbers(numForSum);
+//        Приветствовать любого пользователя при вводе его имени через командную строку.
+        System.out.println("What is your name?");
+        String userName = input.nextLine();
+        mainTask.greeting(userName);
+//        Отобразить в окне консоли аргументы командной строки в обратном порядке.
+        mainTask.revert(userName);
 
+//        Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
+        System.out.println("Input required quantity of numbers: ");
+        int quality = input.nextInt();
+        mainTask.workingWithNumbers(quality);
 
+//         Ввести целые числа как аргументы командной строки, подсчитать их сумму (произведение)
+//         и вывести результат на консоль.
+        System.out.println(" Sum of which numbers you want to know ");
+        int numForSum = input.nextInt();
+        mainTask.summingNumbers(numForSum);
 
-        while (monthActive) {
-            System.out.println("What month number is ? ");
-            int monthNumber = input.nextInt();
-            if () {
-                month = mainTask.whatIsMonthIs(monthNumber);
-                System.out.println("Your month is " + month);
+//        Ввести число от 1 до 12. Вывести на консоль название месяца, соответствующего данному числу.
+//        Осуществить проверку корректности ввода чисел.
+        do {
+            System.out.println("Enter a while number please");
+            if (input.hasNextInt()) {
+                monthNum = input.nextInt();
+                monthActive = true;
+            } else {
+                System.out.println("I dont understand you");
+                monthActive = false;
+                input.next();
             }
-                break;
-
         }
+            while (!(monthActive));
+            System.out.println("your month is " + mainTask.whatIsMonthIs(monthNum));
+
+
+
     }
 }
